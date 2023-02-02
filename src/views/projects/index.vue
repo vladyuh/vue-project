baad92cf557228ab742228edcb1de6ffda7c025b
yukhnovski <v.yukhnovski@jetmail.cc>
2023-02-02 10:56:00 +0300

    updated variables

--- src/views/projects/index.vue
+++ src/views/projects/index.vue
@@ -9,18 +9,11 @@
           capitalize
       />
       <div class="projects-page__list">
-        <router-link
-            class="projects-list__item"
-            v-for="(project, key) in projects"
-            :key="key"
-            :to="`${project.url}`"
-        >
-          <div class="projects-list__item-wr">
-            <span class="projects-list__item-link">{{ project?.link }}</span>
-            <section-header class="projects-list__item-title" :level="4" :text="project?.title"/>
-            <p class="projects-list__item-desc" v-html="project?.description"/>
-          </div>
-        </router-link>
+        <project-list-item
+          v-for="(project, index) in projects"
+          :key="index"
+          :project="project"
+        />
       </div>
     </div>
   </section>
@@ -34,14 +27,14 @@
 <script>
 import {mapActions} from "vuex";
 import backTitle from "@/common-components/back-title";
-import SectionHeader from "@/common-components/section-header";
 import Preloader from "@/common-components/preloader";
+import ProjectListItem from "@/views/projects/project-list-item";
 
 export default {
   name: 'ProjectsView',
   components: {
+    ProjectListItem,
     Preloader,
-    SectionHeader,
     backTitle,
   },
   data() {
---
+++ src/views/projects/project-list-item/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link
+      class="projects-list__item"
+      :to="`${project.url}`"
+  >
+    <div class="projects-list__item-wr">
+      <span class="projects-list__item-link">{{ project?.link }}</span>
+      <section-header class="projects-list__item-title" :level="4" :text="project?.title"/>
+      <p class="projects-list__item-desc" v-html="project?.description"/>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import SectionHeader from "@/common-components/section-header";
+
+export default {
+  name: 'project-list-item',
+  components: {
+    SectionHeader,
+  },
+  props: {
+    project: Object
+  },
+}
+</script>
