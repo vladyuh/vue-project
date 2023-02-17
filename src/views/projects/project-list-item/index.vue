66d4a4884ff055d56f3dd3e3c29e71c137e37eb5
DESKTOP-B6Q22FO\vladu <vladuks123.vv@gmail.com>
2023-02-17 21:55:35 +0300

    fixes project

--- src/views/projects/project-list-item/index.vue
+++ src/views/projects/project-list-item/index.vue
@@ -7,17 +7,29 @@
       <span class="projects-list__item-link">{{ project?.link }}</span>
       <section-header class="projects-list__item-title" :level="4" :text="project?.title"/>
       <p class="projects-list__item-desc" v-html="project?.description"/>
+      <div class="projects-list__item-icon">
+        <icon
+            class="projects-list__item-icon-bg"
+            icon-name="ic_figure"
+        />
+        <icon
+            class="projects-list__item-icon-symbol"
+            icon-name="ic_arrow-up"
+        />
+      </div>
     </div>
   </router-link>
 </template>
 
 <script>
 import SectionHeader from "@/common-components/section-header";
+import Icon from "@/common-components/icon";
 
 export default {
   name: 'project-list-item',
   components: {
     SectionHeader,
+    Icon
   },
   props: {
     project: Object
